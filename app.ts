b23efa98f109858a95ca4583b62005d549c8efc8
SureshKuchana <kuchana123.suresh@gmail.com>
2022-04-21 17:58:28 +0530

    updated file

--- app.ts
+++ app.ts
@@ -7,5 +7,5 @@ const port = 3000
 app.get("/", (_, res) => res.send("Hello World"))
 
 app.listen(port, () => {
-  console.log(`Application running on port ${port}.`)
+  console.log(`Application running on port http://localhost:${port}.`)
 })
